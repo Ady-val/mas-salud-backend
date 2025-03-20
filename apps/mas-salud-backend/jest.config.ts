fe56e3b9041656d62918874a5100dc48646a725c
Adal Valles <adal.vallesb@gmail.com>
2025-03-20 09:49:25 -0600

    Initial commit

---
+++ apps/mas-salud-backend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'mas-salud-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/mas-salud-backend',
+};

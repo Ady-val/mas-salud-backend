27f7d91a60a4f3a199c4d477fcdf0fd08c148861
Adal Valles <adal.vallesb@gmail.com>
2025-03-20 12:25:00 -0600

    feat: creating base modules for institutions and users

--- mas-salud-backend/src/app/app.module.ts
+++ mas-salud-backend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InstitutionsModule } from './institutions/institutions.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [InstitutionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })

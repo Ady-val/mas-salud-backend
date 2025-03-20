ea330996249cce37797bfed9894b148eb12c462c
Adal Valles <adal.vallesb@gmail.com>
2025-03-20 12:12:23 -0600

    feat: creating nest backend project

--- mas-salud-backend/jest.config.ts
+++ mas-salud-backend/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   displayName: 'mas-salud-backend',
-  preset: '../../jest.preset.js',
+  preset: '../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/mas-salud-backend',
+  coverageDirectory: '../coverage/mas-salud-backend',
 };
